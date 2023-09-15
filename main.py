49c789d78e612261e19a8bef16b7d9939256eb84
seo_jangho@radcns.com <seo_jangho@radcns.com>
2023-09-16 00:33:00 +0900

    - 20230916 서장호 작업중

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
-import numpy as np
+import pandas as pd
+from collections import Counter
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import TfidfVectorizer
 
+# 욕설 데이터셋을 읽어옵니다.
+df = pd.read_csv("./data.csv", encoding='cp949')
 
-def random(name):
-    # random_num_a = np.random.randint(-3, 3, 1000)
-    # random_num_b = np.random.rand(1000)
-    # result = random_num_b + random_num_a
+# 욕설 데이터셋을 학습 데이터셋과 테스트 데이터셋으로 나눕니다.
+X_train, X_test, y_train, y_test = train_test_split(
+    df["A"], df["B"], test_size=0.2, random_state=42
+)
 
-    a = -3
-    b = 3
-    result = (b-a) * np.random.rand(1000) + a
+# TF-IDF 벡터화 객체를 생성합니다.
+tfidf_vectorizer = TfidfVectorizer()
 
-    print(f'{result}')
+# 학습 데이터와 테스트 데이터에 TF-IDF 변환을 적용합니다.
+X_train_tfidf = tfidf_vectorizer.fit_transform(X_train)
+X_test_tfidf = tfidf_vectorizer.transform(X_test)
 
+# 욕설 분류 모델을 생성합니다.
+model = LogisticRegression()
 
-if __name__ == '__main__':
-    random('d')
+# 모델을 학습합니다.
+model.fit(X_train_tfidf, y_train)
+
+# 테스트 데이터셋에서 모델의 성능을 평가합니다.
+print(model.score(X_test_tfidf, y_test))
